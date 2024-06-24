e20a5030df05dd60f799fccfa4f3733743bb72bb
Goksel Eryigit <goksel@carbonhealth.com>
2024-06-24 11:43:35 +0300

    tests

--- src/__tests__/page.test.tsx
+++ src/__tests__/page.test.tsx
@@ -1,13 +1,16 @@
 import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
+import {act, render, screen} from "@testing-library/react";
 import Page from "../app/page";
+import {renderWithProviders} from "@/utils/test-utils";
 
 describe("Page", () => {
-  it("renders a heading", () => {
-    render(<Page />);
+  it("renders a heading", async () => {
+    await act( async () => renderWithProviders(<Page/>));
+
+    screen.debug();
 
     const heading = screen.getByRole("heading", { level: 1 });
-
+    //
     expect(heading).toBeInTheDocument();
   });
 });
